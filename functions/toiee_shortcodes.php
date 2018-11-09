229d4f903aafc693e2513407fc71bec28ebe3c09
T. Kameda <kame@hokuken.com>
2018-11-09 18:17:52 +0900

    プレビューを出すためのショートコードを追加
    ミスって消した。作り直して、シンプルにはなった。

--- functions/toiee_shortcodes.php
+++ functions/toiee_shortcodes.php
@@ -5,6 +5,157 @@
  *
  */
  
+ //! 商品プレビューを出すためのショートコード（二回め！）
+ add_shortcode( 'toiee_preview_list' , function ( $atts, $content = null ) {
+	 
+	extract( 
+		shortcode_atts (
+			array(
+				'series' => '',
+				'open' => '1-4',
+				'free' => '5-7'
+			),
+			$atts
+		)
+	); 
+	
+	// parameter check
+	$no = array();
+	foreach( array('open', 'free' ) as $key ) {
+		if( preg_match('/^([0-9]+)-([0-9]+)$/', $$key , $matches) ) {
+			$no[ $key ]['s'] = $matches[1];
+			$no[ $key ]['e'] = $matches[2];
+		}
+		else{
+			return '<p>invalid number of '.$$key.'. like this (1-5)</p>';
+		}
+	}
+	
+//	return "<pre>" .print_r($no, true). "</pre>";
+	
+	
+	// termのチェック
+	if( $series == '' ){ return "<p>please set series slug or id</p>"; }
+	
+	if( is_numeric( $series ) ) {
+		$term = get_term_by('id', $series, 'series', ARRAY_A);
+	}
+	else {
+		$term = get_term_by('slug', $series, 'series', ARRAY_A);
+		if( is_wp_error( $term ) ){
+			$term = get_term_by('name', $series, 'series', ARRAY_A);
+		}
+	}
+	
+	if( is_wp_error( $term ) ){ return "<p>not found series</p>"; }
+	
+//	return "<pre>" .print_r($term, true). "</pre>";
+	
+	// post の取得
+	$posts = get_posts( array(
+		'post_type' => 'podcast',
+		'posts_per_page'   => -1,
+		'order'     => 'ASC',
+		'post_status' => 'publish',
+		'tax_query' => array(
+			array(
+				'taxonomy' => 'series',
+				'field'    => 'term_id',
+				'terms'    => $term['term_id'],
+			),
+		)
+	));
+	
+//	return "<pre>" .print_r( $posts ,true). "</pre>";
+	
+	global $wcr_ssp;
+	global $ss_podcasting;
+	
+	
+	// user check
+	if( is_user_logged_in() ) {
+		$user_logined = true;
+	}
+	else {
+		$user_logined = false;
+		$modal = $wcr_ssp->get_wc_login_form_modal();	
+	}
+	
+	$cnt = 1; $content = '';
+	foreach( $posts as $e ) {
+		
+		if( $no['open']['s'] <= $cnt && $cnt <= $no['open']['e'] ) {
+			$status = 'open';
+		}
+		else if( $no['free']['s'] <= $cnt && $cnt <= $no['free']['e'] ) {
+			if( $user_logined ){
+				$status = 'open';
+			}
+			else {
+				$status = 'free';
+			}
+		}
+		else{
+			$status = 'close';
+		}
+		
+		// get type (audio or video)
+        $episode_type = $ss_podcasting->get_episode_type( $e->ID );
+                        		
+		switch ( $status ) {
+			
+			// プレイヤーを表示
+			case 'open':
+								// get audio file
+				$audio_file = $ss_podcasting->get_enclosure( $e->ID );
+                if ( get_option( 'permalink_structure' ) ) {
+                    $enclosure = $ss_podcasting->get_episode_download_link( $e->ID );
+                } else {
+                    $enclosure = $audio_file;
+                }		
+                $enclosure = apply_filters( 'ssp_feed_item_enclosure', $enclosure, $e->ID );
+            
+                if ( $episode_type == 'audio' ) {
+					$shortcode = '[audio src="'.$enclosure.'" /]';
+				}
+				else {
+					$shortcode = '[video src="'.$audio_file.'" /]';
+				}
+				
+				$content .= "<h4>{$e->post_title}</h4>\n"
+						. do_shortcode( $shortcode )
+						. apply_filters( 'the_content', $e->post_content );
+			
+				break;
+				
+			
+			
+			
+			// ダミーを表示
+			case 'free':
+				$content .= "<h4>{$e->post_title}</h4>\n"
+						. $wcr_ssp->get_dummy_player( $episode_type )
+						. '<p style="font-size:0.8rem;">無料登録(あるいはログイン)することで、ご覧いただけます。<br>
+						<a href="#" uk-toggle="target: #modal_login_form">無料登録する or ログインする場合は、こちらをクリック</a></p>';
+			
+				break;
+				
+			default:
+				$content .= "<p>{$e->post_title}</p>\n";
+		}
+		
+		$content .= "<hr>";
+
+		$cnt++;
+	}
+	
+	return $content.$modal;
+	
+} );
+ 
+ 
+ 
+ 
  //! Podcastの一覧を出力する
  // [toiee_list_series] で、ポケてらを検索して表示
  // [toiee_list_series search="^耳デミー"] で、耳デミーを検索して表示
